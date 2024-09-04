189c32bbf7a82f6b32112e74a3c4cce820939c7e
Efsky <ruzgarhisar7@gmail.com>
2024-09-04 23:13:29 +0300

    Initial Commit

---
+++ ders1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ders1
+//
+//  Created by Efe on 4.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ders1/ders1App.swift
@@ -0,0 +1,17 @@
+//
+//  ders1App.swift
+//  ders1
+//
+//  Created by Efe on 4.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ders1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
